868556e05337f48bac24be8a024f818d6426489c
Alan Colburn <acolburn3@gmail.com>
2025-12-01 15:30:22 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const calcBtn = document.getElementById("calculate-btn");
+const words = document.getElementById("words");
+const lines = document.getElementById("lines");
+const time = document.getElementById("time");
+const speed = document.getElementById("speed");
+const wordsPerPageEl = document.getElementById("words-per-page");
+
+calcBtn.addEventListener("click", function () {
+  const w = words.value;
+  const l = lines.value;
+  const wordsPerPage = (w / 5) * l;
+  let _time = "";
+  let _speed = "";
+  wordsPerPageEl.innerText = `${wordsPerPage} words per page`;
+  for (let i = 60; i > 10; i -= 5) {
+    _time += `<li>${i} sec</li>`;
+    _speed += `<li>${Math.floor((wordsPerPage / i) * 60)} wpm</;i>`;
+  }
+
+  time.innerHTML = _time;
+  speed.innerHTML = _speed;
+});
