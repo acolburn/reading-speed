71a988096a8233ca4730d26913d98f9ea404b013
Alan Colburn <acolburn3@gmail.com>
2025-12-03 11:58:47 -0800

    Added reading speed, timer

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const calcBtn = document.getElementById("calculate-btn");
+const timerBtn = document.getElementById("timer-btn");
 const words = document.getElementById("words");
 const lines = document.getElementById("lines");
 const time = document.getElementById("time");
 const speed = document.getElementById("speed");
 const wordsPerPageEl = document.getElementById("words-per-page");
+let startTime = Date.now();
+let endTime = Date.now();
+let wordsPerPage = 0;
 
-calcBtn.addEventListener("click", function () {
+function calculateWordsPerPage() {
   const w = words.value;
   const l = lines.value;
-  const wordsPerPage = (w / 5) * l;
+  wordsPerPage = (w / 5) * l;
+}
+
+calcBtn.addEventListener("click", function () {
+  calculateWordsPerPage();
   let _time = "";
   let _speed = "";
-  wordsPerPageEl.innerText = `${wordsPerPage} words per page`;
+  // wordsPerPageEl.innerText = `${wordsPerPage} words per page`;
   for (let i = 60; i > 10; i -= 5) {
     _time += `<li>${i} sec</li>`;
     _speed += `<li>${Math.floor((wordsPerPage / i) * 60)} wpm</;i>`;
@@ -19,4 +27,20 @@ calcBtn.addEventListener("click", function () {
 
   time.innerHTML = _time;
   speed.innerHTML = _speed;
+});
+
+timerBtn.addEventListener("click", function () {
+  if (timerBtn.textContent === "START TIMER") {
+    startTime = Date.now();
+    wordsPerPageEl.innerText = "";
+    timerBtn.textContent = "STOP TIMER";
+  } else if (timerBtn.textContent === "STOP TIMER") {
+    endTime = Date.now();
+    const timeDiff = (endTime - startTime) / 1000;
+    calculateWordsPerPage();
+    wordsPerPageEl.innerText = `${timeDiff} sec, ${Math.floor(
+      (wordsPerPage / timeDiff) * 60
+    )} wpm`;
+    timerBtn.textContent = "START TIMER";
+  }
 });
